22a2b461e88ade707c6954b7693298d6bdb4855d
vaidotas <vaidotas.valancius@gmail.com>
2025-04-03 21:10:16 +0300

    Initial commit

---
+++ src/main/java/lt/kurjeriai/kurjeriuuzduotis/KurjeriuUzduotisApplication.java
@@ -0,0 +1,13 @@
+package lt.kurjeriai.kurjeriuuzduotis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurjeriuUzduotisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurjeriuUzduotisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/kurjeriai/kurjeriuuzduotis/KurjeriuUzduotisApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.kurjeriai.kurjeriuuzduotis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurjeriuUzduotisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
